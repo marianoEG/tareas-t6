f6690158aa4ee27b4a13e5a08625ab361ce76b6b
maria <marianen@live.com.ar>
2020-11-02 18:26:56 -0300

    Pequeña modificacion

--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
@@ -17,14 +17,16 @@ class TareaComun(responsable : Empleado, val horasEstimadas : Int, val costoInfr
 }
 
 class TareaIntegracion(val tareas : List<Tarea>, responsable: Empleado) : Tarea(responsable){
+    val horasTrabajoReal = 8
+    val bonusResponsable = 0.03f
 
     override fun horasNecesarias() = tareas.sumBy { it.horasNecesarias() } + this.horasReunion()
 
-    fun horasReunion() = tareas.sumBy { it.horasNecesarias() } / 8
+    fun horasReunion() = tareas.sumBy { it.horasNecesarias() } / horasTrabajoReal
 
     override fun costo() = (tareas.sumBy { it.costo() } + this.bonusResponsable()).toInt()
 
-    fun bonusResponsable() = tareas.sumBy { it.costo() } * 0.03f
+    fun bonusResponsable() = tareas.sumBy { it.costo() } * bonusResponsable
 
     override fun nominaEmpleados() = tareas.flatMap { it.nominaEmpleados() } + responsable
 }
